310fff62808e6c46126843980095dd0f7b8075c4
VladislavZgoda <www.vlad78@yandex.ru>
2026-07-12 15:00:11 +0300

    Add app boilerplate

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
+
+
 def main():
-    print("Hello from not-pulled-last-month!")
+    app = NotPulledLastMonthApp()
+    app.run()
+
+
+class NotPulledLastMonthApp(App):
+    CSS_PATH = "styles.tcss"
+
+    BINDINGS = [
+        ("d", "toggle_dark", "Toggle dark mode"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
 
 
 if __name__ == "__main__":
